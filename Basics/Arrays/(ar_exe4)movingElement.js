f0adf47b4385c7097c961cb4903db5550314d961
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2021-05-31 18:39:33 +0100

    Arrays exercises.

--- Basics/Arrays/(ar_exe4)movingElement.js
+++ Basics/Arrays/(ar_exe4)movingElement.js
@@ -31,9 +31,7 @@ function move(array, index, offset) {
 //My exercise:
 /*
 const numbers = [1, 2, 3, 4];
-
 const output = move(numbers, 0, 1);
-
 console.log(output);
 
 function move(array, index, offset) {
