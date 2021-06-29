ec9195d2f332c5e8694c64eb67547adee9c58776
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2021-06-29 14:11:58 +0100

    Refactoring.

--- Basics/Functions/(fu_exe3)handling_error.js
+++ Basics/Functions/(fu_exe3)handling_error.js
@@ -1,14 +1,14 @@
 //exercise to practice the handling error based in a previous array exercise (ar_exe5 - countOccurrences.js)
 
 try {
-  const numbers2 = true;
-  const count2 = countOccurrences2(numbers2, 1);
-  console.log(count2);
+  const numbers = true;
+  const count = countOccurrences(numbers, 1);
+  console.log(count);
 } catch (e) {
   console.log(e.message);
 }
 
-function countOccurrences2(array, searchElement) {
+function countOccurrences(array, searchElement) {
   if (!Array.isArray(array)) throw new Error("value is not a array");
 
   return array.reduce((accumulator, current) => {
